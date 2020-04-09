9a65a2ba6b1320a637194b8a21235001158be01d
lvzhengbin <lvzhengbin@lizhi.fm>
2020-04-09 09:42:23 +0800

    1.HomeTabFragment 首页转成kotlin实现

---
+++ linkwb/src/main/java/com/ice/wb/fragment/HomeTabFragmentKt.kt
@@ -0,0 +1,65 @@
+package com.ice.wb.fragment
+
+import android.animation.ValueAnimator
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.ice.common.fragment.BaseLazyFragment
+import com.ice.wb.R
+import com.ice.wb.widget.SquareProgressBar
+
+/**
+ * Desc:
+ * Created by lvzhengbin
+ * Time: 2020/4/9
+ */
+class HomeTabFragmentKt : BaseLazyFragment(){
+
+    val TAG : String = "HomeTabFragmentKt"
+    lateinit var mTextview : TextView
+    lateinit var mSquareProgressBar : SquareProgressBar
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.wb_tab_home_fragment, null)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d(TAG, "onViewCreated 首页tab fragment")
+        initView(view)
+        addListener()
+    }
+
+    private fun initView(view : View) {
+        mTextview = view.findViewById(R.id.fragment_tag)
+        mTextview.setText("首页tab fragment页")
+        mSquareProgressBar = view.findViewById(R.id.wb_square_progress)
+        mSquareProgressBar.setImage(R.drawable.station_post_default_bg)
+
+        val valueAnimator = ValueAnimator.ofInt(0, 100)
+        valueAnimator.setDuration(4000)
+        valueAnimator.addUpdateListener { valueAnimator1: ValueAnimator ->
+            val animatedValue = valueAnimator1.animatedValue as Int
+            mSquareProgressBar.setProgress(animatedValue)
+        }
+        valueAnimator.start()
+    }
+
+    private fun addListener(){
+
+    }
+
+    override fun setUserVisibleHint(isVisibleToUser: Boolean) {
+        Log.d(TAG, "setUserVisibleHint() 首页tab isVisibleToUser = $isVisibleToUser")
+        super.setUserVisibleHint(isVisibleToUser)
+    }
+
+    override fun onLazyLoad() {
+        super.onLazyLoad()
+        Log.d(TAG, "onLazyLoad() 首页tab fragment")
+        mTextview.setText("首页tab fragment页 onLazyLoad()")
+    }
+}
