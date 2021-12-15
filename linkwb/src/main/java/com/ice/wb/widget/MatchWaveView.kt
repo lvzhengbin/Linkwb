1346f472118fcb63a1a2e16aef7b5767b2153d8a
lvzhengbin <lvzhengbin@lizhi.fm>
2021-12-15 16:40:45 +0800

    feat: 重写波纹动效，修改为属性动画方案实现

--- linkwb/src/main/java/com/ice/wb/widget/MatchWaveView.kt
+++ linkwb/src/main/java/com/ice/wb/widget/MatchWaveView.kt
@@ -1,15 +1,17 @@
 package com.ice.wb.widget
 
-import android.animation.Animator
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
-import android.view.animation.DecelerateInterpolator
+import android.view.animation.*
 import com.ice.common.utils.SystemUtil
 import com.ice.wb.R
+
 
 /**
  * Desc:  </p>
@@ -21,11 +23,7 @@ class MatchWaveView @JvmOverloads constructor(context: Context,
                                               defStyleAttr: Int = 0): View(context, attrs, defStyleAttr){
 
     private var mPaint: Paint = Paint()
-    private var mRadius = 0f
-
-    private var mValueAnimator: ValueAnimator? = null
-    private var progress = 0
-    private var mIsWave = false
+    private lateinit var animatorSet: AnimatorSet
 
     companion object{
         const val ANIMATION_TIME = 2000L
@@ -36,79 +34,61 @@ class MatchWaveView @JvmOverloads constructor(context: Context,
         mPaint.color = getContext().resources.getColor(R.color.theme_default_color)
         mPaint.strokeWidth = SystemUtil.dip2pxWithFloat(getContext(), 0.8f)
         mPaint.style = Paint.Style.STROKE
+        initAnimation()
+    }
+
+    private fun initAnimation(){
+        val alphaAnimator = ObjectAnimator.ofFloat(this, "alpha", 1f, 0f).apply {
+            repeatMode = ValueAnimator.RESTART
+            repeatCount = ValueAnimator.INFINITE
+        }
+        val scaleXAnimator = ObjectAnimator.ofFloat(this, "scaleX", 0.2f, 1f).apply {
+            repeatMode = ValueAnimator.RESTART
+            repeatCount = ValueAnimator.INFINITE
+        }
+        val scaleYAnimator = ObjectAnimator.ofFloat(this, "scaleY", 0.2f, 1f).apply {
+            repeatMode = ValueAnimator.RESTART
+            repeatCount = ValueAnimator.INFINITE
+        }
+        animatorSet = AnimatorSet().apply {
+            playTogether(alphaAnimator, scaleXAnimator, scaleYAnimator)
+            duration = ANIMATION_TIME
+            interpolator = DecelerateInterpolator()
+        }
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         val cx = width / 2
         val cy = height / 2
-        val maxRadius = Math.min(cx, cy)
-
-        if (progress * maxRadius > 0) {
-            mPaint.alpha = 255 - ((progress - 30) * 255 / (100 - 30))
-            mRadius = (progress * maxRadius / 100).toFloat()
-            canvas.drawCircle(cx.toFloat(), cy.toFloat(), mRadius, mPaint)
-        }
-        if(mIsWave){
-            invalidate()
-        }
+        val maxRadius = Math.min(cx, cy).toFloat()
+        canvas.drawCircle(cx.toFloat(), cy.toFloat(), maxRadius, mPaint)
     }
 
     fun playAnimation() {
-        mIsWave = true
-        invalidate()
-        if (mValueAnimator == null){
-            mValueAnimator = ValueAnimator.ofInt(30, 100).apply {
-                duration = ANIMATION_TIME
-                interpolator = DecelerateInterpolator()
-                repeatMode = ValueAnimator.RESTART
-                repeatCount = ValueAnimator.INFINITE
-                addUpdateListener{ animation ->
-                    progress = animation.animatedValue as Int
-                    //invalidate()
-                }
-                addListener(object : Animator.AnimatorListener {
-                    override fun onAnimationStart(animation: Animator) {
-                        visibility = VISIBLE
-                    }
-
-                    override fun onAnimationEnd(animation: Animator) {
-                        visibility = GONE
-                    }
-
-                    override fun onAnimationCancel(animation: Animator) {
-                        visibility = GONE
-                    }
-
-                    override fun onAnimationRepeat(animation: Animator) {}
-                })
-                start()
-            }
+        if (animatorSet.isPaused){
+            animatorSet.resume()
         }else{
-            mValueAnimator?.let {
-                if (it.isPaused){
-                    it.resume()
-                }
-            }
+            animatorSet.start()
         }
     }
 
     fun pauseAnimation(){
-        mIsWave = false
-        if (mValueAnimator?.isRunning == true){
-            mValueAnimator?.pause()
+        if (animatorSet.isRunning){
+            animatorSet.pause()
         }
     }
 
     fun cancelAnimation(){
-        mIsWave = false
-        mValueAnimator?.let {
-            it.cancel()
-        }
+        animatorSet.cancel()
     }
 
     fun isAnimating(): Boolean{
-        return mIsWave
+        return if (animatorSet.isStarted.not()){
+            false
+        }else{
+            animatorSet.isPaused.not()
+        }
     }
 
 
